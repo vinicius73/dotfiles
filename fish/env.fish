e609febe4879911993e82e48aa65861cc933b7a8
Vinicius Reis <luiz.vinicius73@gmail.com>
2017-08-10 13:21:10 -0300

    update

--- fish/env.fish
+++ fish/env.fish
@@ -1,5 +1,5 @@
-set -gx PATH ~/.npm-global/bin ~/.config/yarn/global/node_modules/.bin $PATH
-# set -gx PATH ~/sdk/android-sdk-linux/platform-tools ~/sdk/android-sdk-linux/build-tools/25.0.2/ ~/sdk/android-sdk-linux/tools $PATH
+set -gx PATH ~/.npm-global/bin ~/.config/yarn/global/node_modules/.bin /opt/android-sdk/build-tools/26.0.1/ $PATH
+# set -gx PATH /opt/android-sdk/platform-tools /opt/android-sdk/build-tools/26.0.1/ /opt/android-sdk/tools $PATH
 
 #set -gx JAVA_HOME /usr/lib/jvm/java-8-openjdk
-#set -gx ANDROID_HOME ~/sdk/android-sdk-linux/
+#set -gx ANDROID_HOME /opt/android-sdk/
