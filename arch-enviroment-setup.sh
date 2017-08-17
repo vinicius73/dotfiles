3d888b5e679110729c35b3eabba0a2f0bdcf9a94
Vinicius Reis <luiz.vinicius73@gmail.com>
2017-08-17 17:51:10 -0300

    adjusts

--- arch-enviroment-setup.sh
+++ arch-enviroment-setup.sh
@@ -1,12 +1,16 @@
 # https://www.ostechnix.com/install-yaourt-arch-linux/
 
 sudo pacman -S --needed base-devel git wget yajl --noconfirm && \
+yaourt -S sublime-text-dev smartgit telegram-desktop-bin && \
+yaourt -S nodejs npm  yarn && \
 yaourt -S java-environment=8 --noconfirm && \
-yaourt -S android-platform android-sdk android-sdk-platform-tools android-sdk-build-tools --noconfirm && \
+yaourt -S android-platform android-sdk android-sdk-platform-tools android-sdk-build-tools gradle --noconfirm && \
 yaourt -S docker docker-compose --noconfirm && \
 yaourt -S ttf-font powerline powerline-fonts fira-code-git screenfetch figlet --noconfirm && \
 yaourt -S yarn --noconfirm
 
 
 sudo groupadd docker && \
-sudo usermod -aG docker $USER
+sudo usermod -aG docker $USER && \
+sudo systemctl enable docker && \
+sudo systemctl start docker
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
+bash arch-enviroment-setup.sh && \
 ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish && \
 rm -rf ~/.config/terminator && \
 ln -s ~/dotfiles/terminator/ ~/.config/terminator && \
 mv ~/.bashrc ~/.bashrc_original && \
-ln -s ~/dotfiles/bash/.bashrc ~/.bashrc && \
-bash arch-enviroment-setup.sh
+ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
