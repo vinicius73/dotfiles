f7badee56c4dae39acfd1943360b30dc1153fa17
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-02-07 12:11:47 -0200

    install pokemonsay

---
+++ install-pokemonsay.sh
@@ -0,0 +1,3 @@
+git clone http://github.com/possatti/pokemonsay --depth=1 && \
+cd pokemonsay && \
+bash install.sh
--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish && \
 rm -rf ~/.config/terminator && \
 ln -s ~/dotfiles/terminator/ ~/.config/terminator && \
 mv ~/.bashrc ~/.bashrc_original && \
-ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
+ln -s ~/dotfiles/bash/.bashrc ~/.bashrc && \
+mkdir ~/bin && \
+bash install-pokemonsay.sh
