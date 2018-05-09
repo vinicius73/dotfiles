937b9d226d95c67ccdd3515594884facb3f7ce3f
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-05-09 17:04:00 -0300

    prevent path errors

--- fish/env.fish
+++ fish/env.fish
@@ -4,7 +4,10 @@ set -gx PATH ~/projects/go/bin $PATH
 
 set -gx GOPATH ~/projects/go
 
-set -gx PATH /opt/android-sdk/build-tools/27.0.3/ $PATH 
-set -gx PATH /opt/android-sdk/platform-tools /opt/android-sdk/build-tools/27.0.3/ /opt/android-sdk/tools $PATH
-set -gx JAVA_HOME /usr/lib/jvm/java-8-openjdk
 set -gx ANDROID_HOME /opt/android-sdk/
+set -gx JAVA_HOME /usr/lib/jvm/java-8-openjdk
+
+if test -d /opt/android-sdk
+  set -gx PATH /opt/android-sdk/build-tools/27.0.3/ $PATH
+  set -gx PATH /opt/android-sdk/platform-tools /opt/android-sdk/build-tools/27.0.3/ /opt/android-sdk/tools $PATH
+end
