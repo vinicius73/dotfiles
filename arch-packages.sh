5033a51a72dd79f89c169054e16ed41f5f40bcfe
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-05-09 16:43:17 -0300

    add new package fzf

--- arch-packages.sh
+++ arch-packages.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 # https://www.ostechnix.com/install-yaourt-arch-linux/
 # https://aur.archlinux.org/packages/yay-bin/
 
 sudo pacman -Sy --needed base-devel git wget yajl --noconfirm  && \
 yaourt -S yay-bin
-yay -S terminator fish cowsay htop --noconfirm  && \
+yay -S terminator fish fzf cowsay htop --noconfirm  && \
 yay -S smartgit telegram-desktop --noconfirm  && \
 yay -S sublime-text-dev atom visual-studio-code-bin --noconfirm  && \
 yay -S nodejs npm yarn --noconfirm  && \
