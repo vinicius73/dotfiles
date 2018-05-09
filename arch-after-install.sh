72e0c932cf55529e2147ec88234c926e53fe145f
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-05-09 17:04:33 -0300

    improve and fix after install

--- arch-after-install.sh
+++ arch-after-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir ~/.npm-global  && \
+mkdir -p ~/.npm-global  && \
 npm config set prefix '~/.npm-global'  && \
 npm -g i http-server  && \
 yarn global add npx  && \
@@ -9,6 +9,6 @@ sudo chown $USER:users /usr/local/bin/  && \
 sudo chown $USER:users /opt && \
 
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher && \
-fish -c "fisher rafaelrinaldi/pure" && \
-fish -c "fisher fisher fzf" && \
-fish -c "fisher edc/bass"
+fish -c "fisher fzf" && \
+fish -c "fisher edc/bass" && \
+fish -c "fisher rafaelrinaldi/pure"
