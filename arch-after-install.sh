f7e9646afac29409f70d7dbe8a35873b9d19edfc
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-05-09 16:42:52 -0300

    improve after install

--- arch-after-install.sh
+++ arch-after-install.sh
@@ -1,7 +1,14 @@
+#!/usr/bin/env bash
+
 mkdir ~/.npm-global  && \
 npm config set prefix '~/.npm-global'  && \
 npm -g i http-server  && \
-yarn global add eslint  && \
+yarn global add npx  && \
 
 sudo chown $USER:users /usr/local/bin/  && \
-sudo chown $USER:users /opt
+sudo chown $USER:users /opt && \
+
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher && \
+fish -c "fisher rafaelrinaldi/pure" && \
+fish -c "fisher fisher fzf" && \
+fish -c "fisher edc/bass"
