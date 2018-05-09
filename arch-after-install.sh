8dd2ead9d409d84a1a25cbaf10e006e4de5e48d4
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-05-09 19:17:40 -0300

    prevent - Node.JS Error: ENOSPC

--- arch-after-install.sh
+++ arch-after-install.sh
@@ -11,4 +11,8 @@ sudo chown $USER:users /opt && \
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher && \
 fish -c "fisher fzf" && \
 fish -c "fisher edc/bass" && \
-fish -c "fisher rafaelrinaldi/pure"
+fish -c "fisher rafaelrinaldi/pure" && \
+
+# https://stackoverflow.com/questions/22475849/node-js-error-enospc/32600959#32600959
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p && \
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.d/99-sysctl.conf && sudo sysctl --system
