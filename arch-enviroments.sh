bec2743b4cf4951b491577ea9440513ec9b8bd2f
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-05-11 11:26:48 -0300

    add sublime configs

--- arch-enviroments.sh
+++ arch-enviroments.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
+
+# fish
 mkdir -p ~/.config/fish && \
-ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish && \
+ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+
+# terminator
 rm -rf ~/.config/terminator && \
-ln -s ~/dotfiles/terminator/ ~/.config/terminator && \
+ln -s ~/dotfiles/terminator/ ~/.config/terminator
+
+# bash
 mv ~/.bashrc ~/.bashrc_original && \
-ln -s ~/dotfiles/bash/.bashrc ~/.bashrc && \
+ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
+
+# sublime
+rm -rf ~/.config/sublime-text-3/Packages/User && \
+ln -s ~/dotfiles/sublime/Packages/User/ ~/.config/sublime-text-3/Packages/User
+
+# anothers
 mkdir -p ~/bin
