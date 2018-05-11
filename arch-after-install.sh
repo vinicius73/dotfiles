5eae5b024a09133b77f2786746d21db0e4d84e82
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-05-11 19:15:01 -0300

    change defalt fish theme

--- arch-after-install.sh
+++ arch-after-install.sh
@@ -11,7 +11,7 @@ sudo chown $USER:users /opt && \
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher && \
 fish -c "fisher fzf" && \
 fish -c "fisher edc/bass" && \
-fish -c "fisher rafaelrinaldi/pure" && \
+fish -c "fisher simple" && \
 
 # https://stackoverflow.com/questions/22475849/node-js-error-enospc/32600959#32600959
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p && \
