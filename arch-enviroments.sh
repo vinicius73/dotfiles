c297f24937d082715e89fddd45b314d93cffb0e3
Vinicius Reis <luiz.vinicius73@gmail.com>
2019-02-08 10:12:23 -0200

    git vlog alias

--- arch-enviroments.sh
+++ arch-enviroments.sh
@@ -16,5 +16,8 @@ ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
 rm -rf ~/.config/sublime-text-3/Packages/User && \
 ln -s ~/dotfiles/sublime/Packages/User/ ~/.config/sublime-text-3/Packages/User
 
+# git
+git config --global alias.vlog 'log --graph --date-order --date=relative --pretty=format:"%C(white)%h: %Cgreen - %an - %Cred %C(cyan)%ar:%Creset%n%s%n" --color'
+
 # anothers
 mkdir -p ~/bin
