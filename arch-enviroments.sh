88cf006544bec4672a9bea3e7ecdce0346eb0969
Vinicius Reis <luiz.vinicius73@gmail.com>
2019-05-13 11:27:04 -0300

    add gitconfig and remove sublime

--- arch-enviroments.sh
+++ arch-enviroments.sh
@@ -13,11 +13,13 @@ mv ~/.bashrc ~/.bashrc_original && \
 ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
 
 # sublime
-rm -rf ~/.config/sublime-text-3/Packages/User && \
-ln -s ~/dotfiles/sublime/Packages/User/ ~/.config/sublime-text-3/Packages/User
+# rm -rf ~/.config/sublime-text-3/Packages/User && \
+# ln -s ~/dotfiles/sublime/Packages/User/ ~/.config/sublime-text-3/Packages/User
 
 # git
 git config --global alias.vlog 'log --graph --date-order --date=relative --pretty=format:"%C(white)%h: %Cgreen - %an - %Cred %C(cyan)%ar:%Creset%n%s%n" --color'
+git config --global user.name "Vinicius Reis"
+git config --global user.email "luiz.vinicius73@gmail.com"
 
 # anothers
 mkdir -p ~/bin
