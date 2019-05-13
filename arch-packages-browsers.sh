74e0c080b689c928b482b19496ad230316f38bfa
Vinicius Reis <luiz.vinicius73@gmail.com>
2019-05-13 11:26:47 -0300

    improve and add packages

---
+++ arch-packages-browsers.sh
@@ -0,0 +1,1 @@
+yay -S google-chrome opera --noconfirm
--- arch-packages.sh
+++ arch-packages.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-# https://www.ostechnix.com/install-yaourt-arch-linux/
 # https://aur.archlinux.org/packages/yay-bin/
 
-sudo pacman -Sy --needed base-devel git wget yajl --noconfirm  && \
-yaourt -S yay-bin
-yay -S terminator fish fzf cowsay htop --noconfirm  && \
-yay -S smartgit telegram-desktop git-extras ctop-bin gotop-bin --noconfirm  && \
-yay -S sublime-text-dev atom visual-studio-code-bin --noconfirm  && \
-yay -S nodejs npm yarn --noconfirm  && \
-yay -S ttf-font powerline powerline-fonts noto-fonts-emoji ttf-fira-code ttf-liberation screenfetch figlet --noconfirm  && \
-yay -S pgadmin3 dbeaver redis-desktop-manager insomnia --noconfirm
+sudo pacman -Sy --needed base-devel git wget yajl --noconfirm && \
+yay -S terminator fish fzf cowsay htop screenfetch figlet --noconfirm && \
+yay -S git-extras ctop-bin gotop-bin --noconfirm && \
+yay -S nodejs npm yarn --noconfirm && \
+yay -S gitkraken sublime-text-dev atom visual-studio-code-bin --noconfirm && \
+yay -S telegram-desktop slack-desktop spotify --noconfirm && \
+yay -S ttf-font powerline powerline-fonts noto-fonts-emoji ttf-fira-code ttf-liberation --noconfirm && \
+yay -S dbeaver insomnia --noconfirm
