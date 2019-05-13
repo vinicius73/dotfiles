76fccf4ddeea14a843b800e2c1db341cee0b06e8
Vinicius Reis <luiz.vinicius73@gmail.com>
2019-05-13 11:27:25 -0300

    fix spaces

--- arch-docker.sh
+++ arch-docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 yay -Sy docker docker-compose --noconfirm
 
-sudo usermod -aG docker $USER  && \
-sudo systemctl enable docker  && \
+sudo usermod -aG docker $USER && \
+sudo systemctl enable docker && \
 sudo systemctl start docker
