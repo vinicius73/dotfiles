69e80dc6ddfdd87b40e61753e74e0871b0c5396d
Vinicius Reis <luiz.vinicius73@gmail.com>
2021-03-30 10:43:22 -0300

    🐛 fix fisher install

--- arch-after-install.sh
+++ arch-after-install.sh
@@ -8,11 +8,11 @@ yarn global add npx  && \
 sudo chown $USER:users /usr/local/bin/  && \
 sudo chown $USER:users /opt && \
 
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher && \
-fish -c "fisher add jethrokuan/fzf" && \
-fish -c "fisher add edc/bass" && \
-fish -c "fisher add franciscolourenco/done" && \
-fish -c "fisher add rafaelrinaldi/pure" && \
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher && \
+fish -c "fisher install jethrokuan/fzf" && \
+fish -c "fisher install edc/bass" && \
+fish -c "fisher install franciscolourenco/done" && \
+fish -c "fisher install rafaelrinaldi/pure" && \
 
 # https://stackoverflow.com/questions/22475849/node-js-error-enospc/32600959#32600959
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p && \
