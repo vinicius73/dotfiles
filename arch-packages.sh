60bf810ef3c1156284241d519ef32a363e929a59
Vinicius Reis <luiz.vinicius73@gmail.com>
2021-11-23 00:56:33 -0300

    🐛 small fix

--- arch-packages.sh
+++ arch-packages.sh
@@ -6,8 +6,8 @@ sudo pacman -Sy --needed base-devel git wget yajl --noconfirm && \
 yay -S terminator fish pv fzf cowsay htop screenfetch figlet --noconfirm --needed && \
 yay -S git-extras ctop-bin gotop-bin --noconfirm --needed && \
 yay -S nodejs npm yarn --noconfirm --needed && \
-yay -S gitkraken sublime-text-dev atom visual-studio-code-bin --noconfirm --needed && \
+yay -S sublime-text-dev atom visual-studio-code-bin --noconfirm --needed && \
 yay -S telegram-desktop slack-desktop spotify --noconfirm --needed && \
 yay -S ttf-font powerline powerline-fonts noto-fonts-emoji ttf-fira-code ttf-liberation --noconfirm --needed && \
-yay -S dbeaver insomnia libratbag piper --noconfirm  --needed && \
+yay -S dbeaver insomnia-bin libratbag piper --noconfirm  --needed && \
 yay -S peco ghq micro --noconfirm --needed
